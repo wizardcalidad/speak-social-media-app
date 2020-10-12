2d95f227923cb5ff5b37f2af92300d714fca835c
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-12 22:34:55 +0100

    a social media application

---
+++ src/main/java/com/speak/SpeakApplication.java
@@ -0,0 +1,13 @@
+package com.speak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speak/SpeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
