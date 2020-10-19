37ef4a229d56accdc6adf3f2989a73375b912aa4
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-19 11:54:34 +0100

    change in the resources

--- src/main/resources/db/create_database_table.sql
+++ src/main/resources/db/create_database_table.sql
@@ -1,23 +1,23 @@
-DROP USER IF EXISTS 'petuser'@'localhost';
-CREATE USER 'petuser'@'localhost' IDENTIFIED BY 'petuser123';
-GRANT ALL PRIVILEGES ON speakdb.* TO 'petuser'@'localhost';
+DROP USER IF EXISTS 'wizardcalidad'@'localhost';
+CREATE USER 'wizardcalidad'@'localhost' IDENTIFIED BY 'wizardcalidad';
+GRANT ALL PRIVILEGES ON speakdb.* TO 'wizardcalidad'@'localhost';
 FLUSH PRIVILEGES ;
 
 DROP DATABASE IF EXISTS speakdb;
 CREATE DATABASE speakdb;
 ENGINE=INNODB;
 
--- CREATE TABLE [IF NOT EXISTS] speak_user (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR (100) NOT NULL,
---     first_name VARCHAR(50) NOT NULL,
---     last_name VARCHAR(50) NOT NULL,
---     email VARCHAR (100) NOT NULL,
---     password VARCHAR (200) NOT NULL,
---     phone_number VARCHAR (20) NULL,
---     address VARCHAR (300) NULL,
---     gender VARCHAR (10) NULL,
---     date_of_birth DATE NOT NULL,
---     date_created TIMESTAMP,
---
--- ) ENGINE=INNODB;
+CREATE TABLE [IF NOT EXISTS] speak_user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR (100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR (100) NOT NULL,
+    password VARCHAR (200) NOT NULL,
+    phone_number VARCHAR (20) NULL,
+    address VARCHAR (300) NULL,
+    gender VARCHAR (10) NULL,
+    date_of_birth DATE NOT NULL,
+    date_created TIMESTAMP,
+
+) ENGINE=INNODB;
