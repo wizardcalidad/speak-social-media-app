58c12d23d65651e81a813959e49f695dc3fa4dcd
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-19 11:37:56 +0100

    application properties updated

--- src/main/java/com/speak/model/User.java
+++ src/main/java/com/speak/model/User.java
@@ -1,16 +1,20 @@
 package com.speak.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="speak")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String first_name;
+    private String last_name;
     private String username;
     private String password;
+    private String emailAddress;
+    private String gender;
+    private String phoneNumber;
+    private String address;
 
 }
--- src/main/resources/db/create_database_table.sql
+++ src/main/resources/db/create_database_table.sql
@@ -5,12 +5,19 @@ FLUSH PRIVILEGES ;
 
 DROP DATABASE IF EXISTS speakdb;
 CREATE DATABASE speakdb;
+ENGINE=INNODB;
 
-CREATE TABLE [IF NOT EXISTS] speak_user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR (100) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR (100),
-
-) ENGINE=INNODB;
+-- CREATE TABLE [IF NOT EXISTS] speak_user (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR (100) NOT NULL,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL,
+--     email VARCHAR (100) NOT NULL,
+--     password VARCHAR (200) NOT NULL,
+--     phone_number VARCHAR (20) NULL,
+--     address VARCHAR (300) NULL,
+--     gender VARCHAR (10) NULL,
+--     date_of_birth DATE NOT NULL,
+--     date_created TIMESTAMP,
+--
+-- ) ENGINE=INNODB;
